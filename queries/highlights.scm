0afd9a6d808944e65a7be393e31868b85345735f
Gilles Roustan <gilles.roustan@mediapart.fr>
2026-05-13 09:47:45 +0200

    feat: update queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,59 +1,81 @@
-(comment) @comment
+(comment) @comment @spell
 
 (filter_identifier) @function.call
+
 (function_identifier) @function.call
+
 (test) @function.builtin
+
 (variable) @variable
+
 (string) @string
+
 (interpolated_string) @string
+
 (operator) @operator
+
 (number) @number
-(boolean) @constant.builtin
+
+(boolean) @boolean
+
 (null) @constant.builtin
+
 (keyword) @keyword
+
 (attribute) @attribute
+
 (tag) @tag
-(conditional) @conditional
-(repeat) @repeat
-(method) @method
-(parameter) @parameter
+
+(conditional) @keyword.conditional
+
+(repeat) @keyword.repeat
+
+(method) @function.method
+
+(parameter) @variable.parameter
 
 [
-    "{{"
-    "}}"
-    "{{-"
-    "-}}"
-    "{{~"
-    "~}}"
-    "{%"
-    "%}"
-    "{%-"
-    "-%}"
-    "{%~"
-    "~%}"
+  "{{"
+  "}}"
+  "{{-"
+  "-}}"
+  "{{~"
+  "~}}"
+  "{%"
+  "%}"
+  "{%-"
+  "-%}"
+  "{%~"
+  "~%}"
 ] @tag.delimiter
 
 [
-    ","
-    "."
-    "?"
-    ":"
-    "="
+  ","
+  "."
 ] @punctuation.delimiter
 
-(interpolated_string [
-    "#{" 
+[
+  "?"
+  ":"
+  "="
+  "|"
+] @operator
+
+(interpolated_string
+  [
+    "#{"
     "}"
-] @punctuation.delimiter)
+  ] @punctuation.special)
 
 [
-    "("
-    ")"
-    "["
-    "]"
-    "{"
+  "("
+  ")"
+  "["
+  "]"
 ] @punctuation.bracket
 
-(hash [
+(hash
+  [
+    "{"
     "}"
-] @punctuation.bracket)
+  ] @punctuation.bracket)
